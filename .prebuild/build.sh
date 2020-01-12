3632125e2820b8a086a2f7d9b655fc0b468ea823
Alexandru Szasz <alexandru.szasz@acquia.com>
2020-01-12 09:41:10 +0200

    added electron 7.1.8

--- .prebuild/build.sh
+++ .prebuild/build.sh
@@ -10,3 +10,4 @@ npm install --only=prod --ignore-scripts
 apt-get install -y g++-multilib gcc-multilib
 prebuild -t 12.4.0 -t 10.16.3 -a x64
 prebuild -t 6.0.10 -r electron -a x64
+prebuild -t 7.1.8 -r electron -a x64
